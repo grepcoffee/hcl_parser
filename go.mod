aa1ef33438ff6bf62060c341e1dd3df465d8b3c5
grepcoffee <grepcoffee@gmail.com>
2021-06-03 13:03:00 -0400

    checkin thurjune3

---
+++ go.mod
@@ -0,0 +1,10 @@
+module main.go
+
+go 1.16
+
+require (
+	github.com/agext/levenshtein v1.2.2 // indirect
+	github.com/hashicorp/hcl/v2 v2.10.0
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -58,32 +58,32 @@ func readHCLFile(filePath string) {
 	//Rename references of variable "a" to "z"
 	for _, block := range f.Body().Blocks() {
 		blockLabels := block.Labels()
-		fmt.Println(blockLabels)
-		blockAttr := block.Body().Attributes()
-		fmt.Println(blockAttr)
-		for _, attr := range f.Body().Attributes() {
-			attr.Expr().RenameVariablePrefix(
-				[]string{"a"},
-				[]string{"z"},
-			)
+		fmt.Println(blockLabels) // Policy Names
+		// blockAttr := block.Body().Attributes()
+		// fmt.Println(blockAttr)
+		// Rename references of variable "a" to "z"
+	}
+}
+
+func replaceHCLFile(filePath string) {
+	contents, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	f, diags := hclwrite.ParseConfig(contents, "", hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		fmt.Printf("errors: %s", diags)
+		return
+	}
+
+	for _, attr := range f.Body().Attributes() {
+		attr.Expr().RenameVariablePrefix(
+			[]string{"advisory"},
+			[]string{"12345678"},
+		)
+	}
 	fmt.Printf("%s", f.Bytes())
-
-	//fmt.Printf("File contents: %s", content)
-
-	// file, diags := hclsyntax.ParseConfig(content, filePath, hcl.Pos{Line: 1, Column: 1})
-	// if diags.HasErrors() {
-	// 	fmt.Println("ParseConfig: %w", diags)
-	// }
-
-	// c := &HclPolicy{}
-	// diags = gohcl.DecodeBody(file.Body, nil, c)
-	// if diags.HasErrors() {
-	// 	fmt.Println("DecodeBody: %w", diags)
-	// }
-
-	//fmt.Println(c)
 }
 
 func main() {
@@ -96,44 +96,44 @@ func main() {
 	// 	}
 	// }
 	readHCLFile("sentinel.hcl")
-
+	replaceHCLFile("sentinel.hcl")
 }
 
-https://pkg.go.dev/github.com/hashicorp/hcl/v2/hclwrite
+// https://pkg.go.dev/github.com/hashicorp/hcl/v2/hclwrite
 
-f := hclwrite.NewEmptyFile()
-rootBody := f.Body()
-rootBody.SetAttributeValue("string", cty.StringVal("bar")) // this is overwritten later
-rootBody.AppendNewline()
-rootBody.SetAttributeValue("object", cty.ObjectVal(map[string]cty.Value{
-	"foo": cty.StringVal("foo"),
-	"bar": cty.NumberIntVal(5),
-	"baz": cty.True,
-}))
-rootBody.SetAttributeValue("string", cty.StringVal("foo"))
-rootBody.SetAttributeValue("bool", cty.False)
-rootBody.SetAttributeTraversal("path", hcl.Traversal{
-	hcl.TraverseRoot{
-		Name: "env",
-	},
-	hcl.TraverseAttr{
-		Name: "PATH",
-	},
-})
-rootBody.AppendNewline()
-fooBlock := rootBody.AppendNewBlock("foo", nil)
-fooBody := fooBlock.Body()
-rootBody.AppendNewBlock("empty", nil)
-rootBody.AppendNewline()
-barBlock := rootBody.AppendNewBlock("bar", []string{"a", "b"})
-barBody := barBlock.Body()
+// f := hclwrite.NewEmptyFile()
+// rootBody := f.Body()
+// rootBody.SetAttributeValue("string", cty.StringVal("bar")) // this is overwritten later
+// rootBody.AppendNewline()
+// rootBody.SetAttributeValue("object", cty.ObjectVal(map[string]cty.Value{
+// 	"foo": cty.StringVal("foo"),
+// 	"bar": cty.NumberIntVal(5),
+// 	"baz": cty.True,
+// }))
+// rootBody.SetAttributeValue("string", cty.StringVal("foo"))
+// rootBody.SetAttributeValue("bool", cty.False)
+// rootBody.SetAttributeTraversal("path", hcl.Traversal{
+// 	hcl.TraverseRoot{
+// 		Name: "env",
+// 	},
+// 	hcl.TraverseAttr{
+// 		Name: "PATH",
+// 	},
+// })
+// rootBody.AppendNewline()
+// fooBlock := rootBody.AppendNewBlock("foo", nil)
+// fooBody := fooBlock.Body()
+// rootBody.AppendNewBlock("empty", nil)
+// rootBody.AppendNewline()
+// barBlock := rootBody.AppendNewBlock("bar", []string{"a", "b"})
+// barBody := barBlock.Body()
 
-fooBody.SetAttributeValue("hello", cty.StringVal("world"))
+// fooBody.SetAttributeValue("hello", cty.StringVal("world"))
 
-bazBlock := barBody.AppendNewBlock("baz", nil)
-bazBody := bazBlock.Body()
-bazBody.SetAttributeValue("foo", cty.NumberIntVal(10))
-bazBody.SetAttributeValue("beep", cty.StringVal("boop"))
-bazBody.SetAttributeValue("baz", cty.ListValEmpty(cty.String))
+// bazBlock := barBody.AppendNewBlock("baz", nil)
+// bazBody := bazBlock.Body()
+// bazBody.SetAttributeValue("foo", cty.NumberIntVal(10))
+// bazBody.SetAttributeValue("beep", cty.StringVal("boop"))
+// bazBody.SetAttributeValue("baz", cty.ListValEmpty(cty.String))
 
-fmt.Printf("%s", f.Bytes())
+// fmt.Printf("%s", f.Bytes())
